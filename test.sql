d8f13ddfa31620cc9755b47713fd76dc9e14c947
ruchira02 <57717796+ruchira02@users.noreply.github.com>
2021-03-29 23:11:04 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,13 @@
+create table studentData(name varchar(30), roll_number int primary key, address varchar(40), mobile_number numeric(10), pan_number varchar(10));
+
+insert into studentData values('Dinesh Oswal',101,'pune',7221829736,'RQDF17671T');
+insert into studentData values('Tina Chavan',102,'pune',9732654544,'FJHSA98569');
+insert into studentData values('Sameer Patil',103,'nagar',8298373436,'UTGBP05454');
+insert into studentData values('Priyanka Dube',104,'navi mumbai',7973923206,'RUBFZ15683');
+insert into studentData values('Kavya Kharat',105,'sangli',9834846458,'KJVDQ74569');
+insert into studentData values('Purva Shinde',106,'kolhapur',9725874664,'MGDTF96581');
+insert into studentData values('Shubham Shinde',107,'mumbai',9654782545,'BVQYH10249');
+insert into studentData values('Kalyani Bhalerao',108,'nagar',9011257866,'GBREP56104');
+insert into studentData values('Mohini Jain',109,'sangli',8745006566,'VCDTY5244G');
+
+
